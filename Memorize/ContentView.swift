e9ed7cf63842d70e2e415c27ec046a5bee3021ae
Gustavo Tiecker <gustavotiecker@gmail.com>
2020-07-02 16:45:41 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Gustavo Tiecker on 02/07/20.
+//  Copyright © 2020 Gustavo Tiecker. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
